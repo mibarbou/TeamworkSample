d565789ee890e10d636a8b346668ee2507b24384
Michel Barbou Salvador <mbarbou@paradigmadigital.com>
2018-01-18 09:24:14 +0100

    - Initial project. - .gitignore. - Cocoapods (Alamofire, Gloss).

---
+++ TeamworkSample/TeamworkSample/Views/ProjectCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProjectCell.swift
+//  TeamworkSample
+//
+//  Created by Michel Barbou Salvador on 18/01/2018.
+//  Copyright © 2018 Michel Barbou Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ProjectCell: UITableViewCell {
+    static let identifier = "ProjectCell"
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
